a2eb9c5fc260584d0889d41db8c7b5a67dadb722
Linette V Maliakal <lmaliakal21@gmail.com>
2018-09-19 01:56:51 -0500

    added bfb project into site
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

---
+++ js/brew.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var y = 3;
+    var x = document.getElementById("myNumber").value;
+    var z = Math.floor(x/y);
+    document.getElementById("demo").innerHTML = "You entagled "+z+" species of marine life.";
+}
